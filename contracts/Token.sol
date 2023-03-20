758e4fa01d74f0e4471d181bec6b7146d9c1927a
David Pineda <david.pineda.orvc@gmail.com>
2023-03-20 15:41:02 -0400

    Replace Token contract and test - prev work breaks on multi token scenario

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,26 +1,15 @@
-// SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-struct TknDeploymentArgs {
-    string _name;
-    string _symbol;
-    uint256 _decimals;
-    uint256 _totalSupply;
-}
-
 contract Token {
     string public name;
     string public symbol;
-    uint256 public decimals;
+    uint256 public decimals = 18;
     uint256 public totalSupply;
 
-    // Track Balance
     mapping(address => uint256) public balanceOf;
-
-    // Track Allowance
-    // allowance(o, s) => [owners: [spenders: [!]]]
     mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(
@@ -28,35 +17,32 @@ contract Token {
         address indexed to,
         uint256 value
     );
-    
+
     event Approval(
         address indexed owner,
         address indexed spender,
         uint256 value
     );
 
-    constructor(TknDeploymentArgs memory args) {
-        name = args._name;
-        symbol = args._symbol;
-        decimals = args._decimals;
-        totalSupply = args._totalSupply * (10**decimals); // << in decimals
-        // ! Assign total supply to deployer
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals);
         balanceOf[msg.sender] = totalSupply;
     }
 
-    // Send Tokens
     function transfer(address _to, uint256 _value)
         public
-        returns(bool success)
+        returns (bool success)
     {
-        bool userHasEnoughToSend;
+        require(balanceOf[msg.sender] >= _value);
 
-        // ! Verify that sender has enough tokens to spend
-        userHasEnoughToSend = balanceOf[msg.sender] >= _value;
-        require(userHasEnoughToSend);
-        
         _transfer(msg.sender, _to, _value);
-        
+
         return true;
     }
 
@@ -65,23 +51,18 @@ contract Token {
         address _to,
         uint256 _value
     ) internal {
-        // ! Reject invalid recipient
         require(_to != address(0));
 
-        // Deduct token balance from sender
         balanceOf[_from] = balanceOf[_from] - _value;
-        // Credit tokens to receiver
         balanceOf[_to] = balanceOf[_to] + _value;
 
         emit Transfer(_from, _to, _value);
     }
 
-    // APPROVAL
     function approve(address _spender, uint256 _value)
         public
         returns(bool success)
     {
-        // ! Reject invalid spender
         require(_spender != address(0));
 
         allowance[msg.sender][_spender] = _value;
@@ -94,18 +75,18 @@ contract Token {
         address _from,
         address _to,
         uint256 _value
-    ) public returns (bool success) {
-
-        // check has amount available
+    )
+        public
+        returns (bool success)
+    {
         require(_value <= balanceOf[_from]);
-        // check approval - DO NOT transfer more than the approved value spec
         require(_value <= allowance[_from][msg.sender]);
 
-        // RESET allowance incrementally
-        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value; 
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
 
-        // XFER
         _transfer(_from, _to, _value);
+
         return true;
     }
+
 }
