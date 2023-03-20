c25f7cd543f925c4c698256e83e72bee977595bf
David Pineda <david.pineda.orvc@gmail.com>
2023-03-20 14:30:24 -0400

    Start AMM.sol contract

---
+++ contracts/AMM.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+// [] Manage Pool
+// [] Manage Deposits
+// [] Facilitate Swaps (i.e. trades)
+// [] Manage Withdraws
+
+contract AMM {
+
+}
