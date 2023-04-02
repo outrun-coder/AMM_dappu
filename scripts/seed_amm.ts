9000ed2ac6a8e8472543c63a32723a4e835db602
David Pineda <david.pineda.orvc@gmail.com>
2023-04-02 12:05:13 -0400

    start the seed script

---
+++ scripts/seed_amm.ts
@@ -0,0 +1,11 @@
+const hre = require("hardhat");
+const config = require("../src/config.json");
+
+async function main() {
+
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
