17b65a61f4a3b54b4ca0a88b7dfbdd10dc53b3a8
David Pineda <david.pineda.orvc@gmail.com>
2023-04-02 13:03:10 -0400

    Fetch the AMM contract and condition ditribution of tokens to investors

--- scripts/seed_amm.ts
+++ scripts/seed_amm.ts
@@ -9,7 +9,7 @@ const ether = tokens;
 const shares = ether;
 
 async function main() {
-  // Fetch accounts
+  // ! Fetch accounts
   console.log(`>> Fetching accounts & networks \n`);
   const accounts = await ethers.getSigners();
   const [
@@ -20,19 +20,46 @@ async function main() {
     investor_4,
   ] = accounts;
 
-  // Fetch Network Config
+  // ! Fetch Network Config
   const { chainId } = await ethers.provider.getNetwork();
   const network = config[chainId];
   console.log(`>> Using network:`, network);
 
-  // Fetch Series Contracts
+  // ! Fetch Series Contracts
   const dappuTokenContract = await ethers.getContractAt('Token', network.dappu.address);
   console.log(`>> DAPPU Token contract fetched: ${dappuTokenContract.address} \n`);
   
 
   const musdcTokenContract = await ethers.getContractAt('Token', network.musdc.address);
   console.log(`>> MUSDC Token contract fetched: ${musdcTokenContract.address} \n`);
+
+
+  const ammContract = await ethers.getContractAt('AMM', network.amm.address);
+  console.log(`>> AMM Token contract fetched: ${ammContract.address} \n`);
   
+  // // // // //
+  // ! Dist. tokens to Investors
+  console.log(`>> Dist tokens to investors... \n`);
+  
+  let trx;
+  const willDistribute = false;
+  
+  if (willDistribute) {
+    console.log(`>> Distributing \n`);
+      // i1 - dappu
+    trx = await dappuTokenContract.connect(deployer).transfer(investor_1.adderss, tokens(10));
+    await trx.wait();
+    // i2 - musdc
+    trx = await musdcTokenContract.connect(deployer).transfer(investor_2.adderss, tokens(10));
+    await trx.wait();
+    
+    // i3 - dappu
+    trx = await dappuTokenContract.connect(deployer).transfer(investor_3.adderss, tokens(10));
+    await trx.wait();
+    // i4 - musdc
+    trx = await musdcTokenContract.connect(deployer).transfer(investor_4.adderss, tokens(10));
+    await trx.wait();
+}
 }
 
 main().catch((err) => {
