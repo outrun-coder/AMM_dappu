22d06317fdb2e4d25c98a5a067c9aff7a0256082
David Pineda <david.pineda.orvc@gmail.com>
2023-04-02 12:21:11 -0400

    Fetching signer accounts

--- scripts/seed_amm.ts
+++ scripts/seed_amm.ts
@@ -1,14 +1,24 @@
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 const config = require("../src/config.json");
 
 
 const tokens = (n) => {
-  return hre.ethers.utils.parseUnits(n.toString(), 'ether');
+  return ethers.utils.parseUnits(n.toString(), 'ether');
 }
 const ether = tokens;
 const shares = ether;
 
 async function main() {
+  // Fetch accounts
+  console.log(`  Fetching accounts & networks \n`);
+  const accounts = await ethers.getSigners();
+  const [
+    deployer,
+    investor_1,
+    investor_2,
+    investor_3,
+    investor_4,
+  ] = accounts;
 
 }
 
