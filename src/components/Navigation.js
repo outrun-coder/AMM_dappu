3763bd9629d28cc565568c50cc482f527b6861a2
David Pineda <david.pineda.orvc@gmail.com>
2023-04-04 21:35:44 -0400

    Format and display of current user account

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { useSelector } from 'react-redux';
+import Blockies from 'react-blockies';
 
 import Navbar from 'react-bootstrap/Navbar';
 
@@ -8,7 +9,7 @@ import logo from './logo.png';
 const Navigation = () => {
   const account = useSelector(state => state.ethersProvider.account);
 
-  const truncatedAccount = `${account.slice(0,5)}...${account.slice(38, 42)}`
+  const truncatedAccount = (account) ? `${account.slice(0,5)}...${account.slice(38, 42)}` : 'N/A';
 
   return (
     <Navbar className='my-3'>
@@ -21,9 +22,21 @@ const Navigation = () => {
       />
       <Navbar.Brand href="#">Dapp University DAO</Navbar.Brand>
       <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text>
-          {truncatedAccount}
-        </Navbar.Text>
+        {(account) ? (
+          <Navbar.Text>
+            {truncatedAccount}
+            <Blockies
+              seed={account}
+              size={10}
+              scale={3}
+              color="#2187D0"
+              bgColor="#F1F2F9"
+              spotColor="#767F92"
+              className="identicon mx-2"/>
+          </Navbar.Text>
+        ) : (
+          <>Loading...</>
+        )}
       </Navbar.Collapse>
     </Navbar>
   );
